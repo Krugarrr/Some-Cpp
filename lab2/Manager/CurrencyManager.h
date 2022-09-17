635c72c89956b643a1682b336c40cc2e29096904
Krugarrr <ttteeework@gmail.com>
2022-09-17 17:08:29 +0300

    add currencymanager

---
+++ lab2/Manager/CurrencyManager.h
@@ -0,0 +1,24 @@
+#ifndef INC_2LABCPP_CURRENCYMANAGER_H
+#define INC_2LABCPP_CURRENCYMANAGER_H
+
+#include "curl/curl.h"
+#include "../nlohmann/json.hpp"
+#include "../CurrencyObject/Currency.h"
+#include "../include/simple_include.h"
+
+
+
+class CurrencyManager {
+private:
+    std::map<std::string, Currency> currency_manager;
+
+public:
+    void request();
+    void print();
+    void start(int request_count, int interval);
+    static void is_changed(Currency currency);
+    static size_t write_data(char *ptr, size_t size, size_t nmember, std::string* data);
+};
+
+
+#endif //INC_2LABCPP_CURRENCYMANAGER_H

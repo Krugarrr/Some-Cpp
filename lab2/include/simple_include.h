04999a087c4922fe76f4903feba74823b5bbd8c4
Krugarrr <ttteeework@gmail.com>
2022-09-17 17:09:32 +0300

    add include

---
+++ lab2/include/simple_include.h
@@ -0,0 +1,24 @@
+#include <windows.h>
+#include <conio.h>
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <clocale>
+#include <vector>
+#include <map>
+#include <cstdlib>
+#include <unistd.h>
+#include <algorithm>
+#include <iostream>
+#include <time.h>
+#include <iomanip>
+#include <queue>
+#include <cmath>
+#include <limits>
+
+
+
+
+
+
+
